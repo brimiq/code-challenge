9049960ca94c2d0c97344bb9021c18cbd6ddd977
bradley <fazebrad13@gmail.com>
2025-09-09 22:21:20 +0300

    Made it a function to test easier

--- speed-detector.js
+++ speed-detector.js
@@ -1,15 +1,19 @@
 let carSpeed=80
-if(carSpeed<70){
-    console.log("Ok")
+
+function carSpeedometer(carSpeed){ 
+    if(carSpeed<70){
+    return "Ok"
 }else if(carSpeed>70){
    //by doing the car speed -70 we establish how much over 70 we are
    //for example if the speed = 80 , 80-70=10 then if we divide that by 5 we get 2
    //hence two demerits(points) will be awarded
     const points = Math.floor((carSpeed-70)/5)
     if(points>12){
-        console.log("Licence terminated")
+       return "Licence terminated"
     }else{
-        console.log("Total points are", points)
+       return `Total points are, ${points}`
     }
 }
+}
 //testing
+console.log(carSpeedometer(80))
